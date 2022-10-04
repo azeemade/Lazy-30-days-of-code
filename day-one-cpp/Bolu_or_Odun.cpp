2aa2977123eac05d7bd97cafd87caa4ba61d1695
Azeem <59115414+azeemade@users.noreply.github.com>
2022-10-04 20:14:08 +0100

    30 days of code, day 1

---
+++ day-one-cpp/Bolu_or_Odun.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+bool check(std::string& str)
+{
+    if(str.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_") != std::string::npos)
+    {
+        return true;
+    }
+}
+
+void print(std::string& str)
+{
+	if(str == "Bolu" || str == "Odun")
+    {
+    	std::cout << "Welcome back " + str;
+    }
+    else
+    {
+    	std::cout << "It is nice to meet you " + str;
+    }
+}
+
+
+int main() {
+	std::string str;
+    
+ 	std::cout << "Enter a word: ";
+    getline(std::cin, str);
+
+    try {
+        if(!check(str))
+        {
+            print(str); 
+        }
+        else
+        {
+            throw 500;
+        }
+    }
+    catch (...) {
+        std::cout << "Please remove all spaces or special characters";
+    }
+    
+  	return 0;
+}
+
